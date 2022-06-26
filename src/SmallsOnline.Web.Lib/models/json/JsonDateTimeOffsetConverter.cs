853f0c84048afc3ecd448d31d866adcb223fa519
Timothy Small <smalls.online@icloud.com>
2022-06-25 20:19:07 -0400

    Updated datetimeoffset

--- src/SmallsOnline.Web.Lib/models/json/JsonDateTimeOffsetConverter.cs
+++ src/SmallsOnline.Web.Lib/models/json/JsonDateTimeOffsetConverter.cs
@@ -2,12 +2,11 @@ namespace SmallsOnline.Web.Lib.Models.Json;
 
 public class JsonDateTimeOffsetConverter : JsonConverter<DateTimeOffset>
 {
-    private readonly TimeZoneInfo _dateTimeTimeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
 
     public override DateTimeOffset Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        DateTime inputTime = DateTime.Parse($"{reader.GetString()}");
-        return new(inputTime, _dateTimeTimeZoneInfo.GetUtcOffset(inputTime));
+        DateTimeOffset inputTime = DateTimeOffset.Parse($"{reader.GetString()}");
+        return inputTime.UtcDateTime;
     }
 
     public override void Write(Utf8JsonWriter writer, DateTimeOffset value, JsonSerializerOptions options)
